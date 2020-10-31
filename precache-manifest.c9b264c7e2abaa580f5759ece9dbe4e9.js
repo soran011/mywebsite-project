d58db69bf3aac80acf07a62ac21f561fb8a1a7e1
soran011 <haruti.soran@gmail.com>
2020-10-30 22:24:32 -0400

    Updates

---
+++ precache-manifest.c9b264c7e2abaa580f5759ece9dbe4e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e73d48166359d0b39499279fdb60856",
+    "url": "/mywebsite-project/index.html"
+  },
+  {
+    "revision": "74ff1021a37fc35276af",
+    "url": "/mywebsite-project/static/css/main.8e66cd5a.chunk.css"
+  },
+  {
+    "revision": "7f7c38015d67fa12b5b5",
+    "url": "/mywebsite-project/static/js/2.af9526eb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mywebsite-project/static/js/2.af9526eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ff1021a37fc35276af",
+    "url": "/mywebsite-project/static/js/main.614222b1.chunk.js"
+  },
+  {
+    "revision": "1ee0e0f5a0aaf07e27f7",
+    "url": "/mywebsite-project/static/js/runtime-main.7d1cd5a1.js"
+  },
+  {
+    "revision": "68992f42d3e6021b8fa0a028564d6837",
+    "url": "/mywebsite-project/static/media/pic-1.68992f42.svg"
+  },
+  {
+    "revision": "72507e368b736205cb47827180fa6f08",
+    "url": "/mywebsite-project/static/media/pic-2.72507e36.svg"
+  },
+  {
+    "revision": "86909172415b29a3effbffd1e54a1556",
+    "url": "/mywebsite-project/static/media/pic-3.86909172.svg"
+  },
+  {
+    "revision": "bcfc668db16038f532680f74f190012d",
+    "url": "/mywebsite-project/static/media/pic-4.bcfc668d.svg"
+  },
+  {
+    "revision": "fcac6f07bbf4f1f3a30e97aadc7bd07a",
+    "url": "/mywebsite-project/static/media/pic-6.fcac6f07.svg"
+  },
+  {
+    "revision": "c2a385d099529b32ecdfbc764c0c5dae",
+    "url": "/mywebsite-project/static/media/video.c2a385d0.mp4"
+  }
+]);
